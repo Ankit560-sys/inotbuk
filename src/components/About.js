5361b06db081c858bf5c631dc99f1e553acd997d
Ankit Rawat <ankitrt456@gmail.com>
2024-03-02 16:18:26 -0600

    added a uselocation for location highlight

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,17 @@
-import React ,{useContext, useEffect} from 'react';
-import noteContext from '../context/notes/noteContext';
+// import React ,{useContext} from 'react';
+// import noteContext from '../context/notes/noteContext';
 
 
 
 
 const About = () => {
 
-   const a = useContext(noteContext);
 
-   useEffect(() => {
-       a.update();
-     // eslint-disable-next-line
-   }, [])
    
   return (
     <div> 
       <h1>
-        This is  About {a.state.name} and he is in {a.state.class}
+        This is  About Page
         </h1></div>
   )
 }
--- src/context/notes/Notestate.js
+++ src/context/notes/Notestate.js
@@ -1,39 +1,22 @@
 import  NoteContext from "./noteContext";
 
-import {useState} from 'react';
+
 
 
 const NoteState = (props) =>{
 
-    const s1 = {
-        
-        "name" : "Ankit",
-        "class" : "5e"
+  
+    
+
+   
 
 
-    }
-
-    const [state , setState] = useState(s1);
-
-    const update =()=>{
-
-        setTimeout(() => {
-
-            setState({
-                "name":"Rahul",
-
-                "class":"41k"
-            })
-            
-        }, 1000);
-
-
-    }
+    
 
     return (
   
 
-         <NoteContext.Provider value={{state ,update}}>
+         <NoteContext.Provider>
 
            {props.children}
 
