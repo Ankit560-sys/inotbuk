516c29cda93d36c51c93e282f7d1ee59c722de6a
Ankit Rawat <ankitrt456@gmail.com>
2024-03-01 18:18:18 -0600

    Components and react router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+
+
 
 function App() {
   return (
-    <div className="App">
-     <h1>this is inotebuk</h1>
+    <>
+    <BrowserRouter>
 
-    </div>
+     <Navbar/>
+    
+
+    <Routes>
+     
+     <Route exact path="/" element={<Home/>}></Route>
+     <Route exact path="/about" element={<About/>}></Route>
+     
+
+
+
+
+    </Routes>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </BrowserRouter>
+
+    </>
   );
 }
 
